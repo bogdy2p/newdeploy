67a1b2058e81d8b1f6a365bccece38d3e6542a9f
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-04-08 15:59:23 +0300

    Modified executedeploy

--- executedeploy.sh
+++ executedeploy.sh
@@ -2,6 +2,6 @@
 
 echo "THIS IS NOT YET IMPLEMENTED";
 echo "YOU MUST CONNECT VIA SSH AND CALL THE COMMAND IN THIS SCRIPT BY TERMINAL";
-#export BUILD_NUMBER=1 && magento-deployscripts/deploy.sh -d -e staging -r artifacts/build-${BUILD_NUMBER}.tar.gz -t ~/htdocs/amorana-v2/staging
+#export BUILD_NUMBER=1 && magento-deployscripts/deploy.sh -d -e latest -r artifacts/build-${BUILD_NUMBER}.tar.gz -t ~/htdocs/amorana-v2/production
 echo "ExecuteDeploy Called Succesfully (mustimplement)";
 
